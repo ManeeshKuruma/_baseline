3262dd0c8a69909a60c6f6900e8cca8b7f33f2b5
D Maneesh <maneesh.d@ggktech.com>
2020-03-19 22:37:24 +0530

    Added Number generator

--- data_generator.py
+++ data_generator.py
@@ -27,8 +27,47 @@ def string_generator(length=None, size=None, unique=False):
             ele_array.append(val)
             i = i+1
     return ele_array
- 
-def number_generator(length=0):
-    return 0
 
-print(string_generator(1, 15, True))
+
+### If length of string is not provided then by default we will generate a string from range of 5-10 length of string
+### If size i.e., number of records is not mentioned we will throw an error
+### If requested for a unique column value with size > max_generations, then we will throw an error
+
+def number_generator(length=None, size=None, unique=False):
+    ele_array= []
+    len_not_provided = False
+    
+    if (length!=None and unique and 9*(10**(length-1)) < size) or size==None:
+        print("error")
+        return []
+    
+    if length==None:
+        len_not_provided=True
+    
+    i=0
+    while i<size:
+        val = ""
+        if len_not_provided:
+            length = randint(2,6)
+        for j in range(length):
+            if j==0:
+                digit = choice("123456789")
+            else:
+                digit = choice("0123456789")
+            val = val + digit
+        if not(unique==True and val in ele_array):
+            ele_array.append(val)
+            i = i+1
+    return ele_array
+
+print(number_generator(None, 10, True))
+print(string_generator(None, 10, True))
+
+#Nullable or NotNullable
+#Negative numbers
+#Int BigInt SmallInt
+#Char VarChar Text
+#Binary Boolean
+#Float Double Decimal
+#Date Time TimeStamp DateTime
+#Serial
